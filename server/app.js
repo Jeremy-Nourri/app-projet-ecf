041653c639bbd592decabb683017db3123982a40
Jeremy-Nourri <nourri.jer@gmail.com>
2024-02-07 11:41:50 +0100

    feat: connect to the database with the ORM Sequelize

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { sequelize } = require('./config/database');
+require('dotenv').config();
+
 const app = express();
 
 const PORT = 3000;
@@ -9,6 +12,12 @@ app.get('*', (req, res) => {
   res.status(404).json({ error: 'Page non trouvée' });
 });
 
+sequelize
+  .authenticate()
+  .then(() => console.log("Connexion avec la base de données réussie"))
+  .catch((err) => {
+    console.log("Erreur lors de la connexion à la base de données", err.message);
+});
 
 app.listen(PORT, () => {
   console.log(`Serveur en écoute sur le port ${PORT}`);
---
+++ server/config/database.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+sequelize
+  .sync({ force: false})
+  .then(() => console.log("La base de données à bien été synchronisée"))
+  .catch((error) =>
+    console.error("Problème lors de la synchronisation :", error.message)
+);
+
+
+module.exports = { sequelize }
